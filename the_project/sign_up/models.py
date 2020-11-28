d9db5f98e629a14f5f9eb8e8ab8a203a6c797aa2
citron-MAC <csri16021@g.nihon-u.ac.jp>
2020-11-28 16:37:46 +0900

    coding  base program

--- the_project/sign_up/models.py
+++ the_project/sign_up/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UserList(models.Model):
+    user_id = models.CharField(max_length=20)
+    mail = models.EmailField(max_length=100)
+    pswd = models.CharField(max_length=20)
--- the_project/the_project/settings.py
+++ the_project/the_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sign_in',
+    'sign_up',
+    'toppage',
 ]
 
 MIDDLEWARE = [
