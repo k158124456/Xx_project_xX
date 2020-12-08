b72434dab59d49ada27c85a4e008a1802e30acd8
citron-MAC <csri16021@g.nihon-u.ac.jp>
2020-12-08 18:09:58 +0900

    招待された際の挙動を全て実装

--- the_project/mainpage/programs/notify.py
+++ the_project/mainpage/programs/notify.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 # Create your views here.
 from django.contrib.auth.decorators import login_required
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 import pandas as pd
 from django.http import HttpResponse
 from ..forms import InviteForm
+from django.urls import reverse
+from urllib.parse import urlencode
 
 class Notify(TemplateView):
     def get(self, request):
@@ -15,7 +17,66 @@ class Notify(TemplateView):
 
         params = {
             "userdata" : str(request.user),
-            "items" : notify_list
+            "items" : notify_list,
+            "message":""
         }
-
+        # リダイレクトされた時に一緒に渡されたクエリパラメータから判断
+        if 'a_project' in request.GET:
+            params["message"] = request.GET["a_project"] + "に加入しました"
+        if 'd_project' in request.GET:
+            params["message"] = request.GET["d_project"] + "への招待を全て拒否しました"
+        if 'message' in request.GET:
+            params["message"] = request.GET["message"] + "にはすでに加入しています。招待を削除しました"
+        
+        
         return render(request, 'mainpage/notify.html', params)
+
+class Accept(TemplateView):
+    def get(self, request):
+        #パラメータのprojectを読み取り
+        projectname = request.GET['project']
+        #projectメンバの中で、招待されているprojectでフィルタし、その上でログインユーザでフィルタリングした時に、中に何も入っていなければ登録される
+        already_recorded = ProjectMember.objects.filter(
+            projectlist=Project.objects.get(project_name=projectname)
+            ).filter(userlist=request.user)
+        
+        #存在しているかどうか
+        recordable = already_recorded
+        #存在しているのであれば、招待されているものを全て消去
+        if recordable:
+            inv = Invite.objects.filter(
+                project_name=Project.objects.get(project_name=projectname)
+                )
+            inv.delete()
+            redirect_url = reverse('mainpage:notify')
+            return redirect(redirect_url+"?message="+projectname)
+
+        #存在しない場合、projectメンバーとして登録される
+        else:
+            pm = ProjectMember(
+                userlist = request.user,
+                projectlist = Project.objects.get(project_name=projectname),
+                displayname = "新参者",
+                role = 0,
+            )
+            pm.save()
+
+            #招待されているものを削除
+            inv = Invite.objects.filter(
+                project_name=Project.objects.get(project_name=projectname)
+                )
+            inv.delete()
+
+            redirect_url = reverse('mainpage:notify')
+            return redirect(redirect_url+"?a_project="+projectname)
+
+class Reject(TemplateView):
+    def get(self, request):
+        redirect_url = reverse('mainpage:notify')
+        projectname = request.GET['project']
+        inv = Invite.objects.filter(
+            project_name=Project.objects.get(project_name=projectname)
+            )
+        inv.delete()
+
+        return redirect(redirect_url+"?d_project="+projectname)
