309ee2d19cf6c2b1d899ddbfe2246640dcc7f674
k158124456 <cska16114@g.nihon-u.ac.jp>
2020-12-23 20:24:16 +0900

    バグ修正(オフライン時にオフラインクリックしたときに他のグループがオフラインになるバグを直した)

--- the_project/grouppage/urls.py
+++ the_project/grouppage/urls.py
@@ -7,6 +7,8 @@ app_name = "roompage"
 urlpatterns = [
     # ここに再接続された地点でmainpage/<projectname>/grouppage/はくっついてる
     path("", views.RoomPage.as_view(), name="roompage"),
-    path("status_edit/",views.Status_new.as_view(),name="status_new"),
     path("group_settings/",views.Group_settings.as_view(),name="group_settings"),
+    path("status_edit/",views.Status_new.as_view(),name="status_new"),
+    path("group_namechange/",views.Group_NameChange.as_view(),name="group_namechange"),
+    path("group_delete/",views.Group_Delete.as_view(),name="group_delete"),
 ]
