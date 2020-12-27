d88ed16455b56bb1ad8fbd23a204cb0df86ac313
citron-MAC <csri16021@g.nihon-u.ac.jp>
2020-12-27 11:14:05 +0900

    同じステータスを登録しようとすると登録できなくなるようにした。

--- the_project/grouppage/view/status_edit.py
+++ the_project/grouppage/view/status_edit.py
@@ -83,12 +83,15 @@ class Status_new(TemplateView):
         status_new = request.POST.get("detail")
         #groupID = request.GET["groupname"]
         projectID = project_id
-        record_status = Status_detail(
-            group_id=group,
-            status_id=1+status_id_new.status_id,
-            detail=status_new,
-        )
-        record_status.save()
+        if Status_detail.objects.get(detail=status_new):
+            message = "同じステータスは登録できません。"
+        else:
+            record_status = Status_detail(
+                group_id=group,
+                status_id=1+status_id_new.status_id,
+                detail=status_new,
+            )
+            record_status.save()
 
         chat = Chat.objects.filter(group_id__uuid=groupID)
 
@@ -105,7 +108,7 @@ class Status_new(TemplateView):
         self.params["status_details"] = status_detail
         self.params["displayname_role"] = d_r[0]
         self.params["title"]=projectname+"/"+groupname+":setting_status_edit"
-
+        self.params["message"] = message
 
         return render(request, 'grouppage/status_edit.html', self.params)
 
